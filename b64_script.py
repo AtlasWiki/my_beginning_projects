c1c10029840a9163170de814a1528e2f2ae5cc84
AtlasWiki <87085506+AtlasWiki@users.noreply.github.com>
2021-07-07 14:57:13 -0700

    Update b64_script.py

--- b64_script.py
+++ b64_script.py
@@ -2,12 +2,10 @@
 # Base64 Encoder/Decoder
 ########### By: AtlasWiki ###########
 # Comment: this is my first python project, hope you enjoy
-# also do not copy the b' ' part
-# that just means byte string
 
 print("Hi, this script was made to encode/decode base64 codes")  
 print("Made by: AtlasWiki \n")
-print("make sure to not copy the b'  ' part")
+
 
 # Entire Base64 Program
 def b64_program():
@@ -18,8 +16,9 @@ def b64_program():
 # If user picks "decode", user will be prompted to input encoded message
     def decode():
         if (user_decision.lower() == "decode"):
-            b64_decode = input ("decode: ") # prompts user to input base64
-            print(base64.b64decode(b64_decode)) # base64 code gets decoded
+            b64_decode = input ("\ndecode_input: ") # prompts user to input base64
+            decoded_text = (base64.b64decode(b64_decode)) # base64 code gets decoded
+            print('decode_output: ' + str(decoded_text).lstrip('b').strip('\'')) # prints decoded output
             input(" \n hit enter to continue        ")
     decode()
 
@@ -27,9 +26,10 @@ def b64_program():
 # If user picks "encode", user will be prompted to input decoded message
     def encode():
         if (user_decision.lower() == "encode"):
-            b64_input = input("encode: ")
-            b64_encoded = bytes(b64_input, encoding = "utf-8") #encodes user-input to byte string
-            print(base64.b64encode(b64_encoded)) # encodes to base64
+            b64_input = input("\nencode_input: ")
+            b_encoded = bytes(b64_input, encoding = "utf-8") #encodes user-input to byte string
+            b64_encoded = (base64.b64encode(b_encoded)) # text gets encoded
+            print('encode_output: ' + str(b64_encoded).lstrip('b').strip('\'')) # prints encoded string
             input(" \n hit enter to continue        ")
     encode()
 
@@ -49,24 +49,24 @@ def b64_program():
  # user is taken back to the entire program when wanting to encode/decode again
 # Asks user if the user wants to encode/decode more codes
 # takes user back to the beginning of the program when wanting to encode/decode again
-    reset_input = input("\n Do you want to decode/encode more stuff? ")
-    if (reset_input.lower() == "yes" or reset_input.lower() == "sure"):
+    reset_input = input("\n Do you want to decode/encode more stuff? (y/n) ")
+    if (reset_input.lower() == "y"):
         b64_program()
 # program ends when user does not want to encode/decode again
-    elif (reset_input.lower() == "no" or reset_input.lower() == "nope"):
+    elif (reset_input.lower() == "n"):
             print("bye, thanks for using this")
             input("press enter to exit")
 # keeps repeating question when user specifies an unknown option
     else:
-        while not (reset_input.lower() == "no" or reset_input.lower() =="nope"):
-            print("that is not even an option")
-            reset_input = input("Do you want to decode/encode more stuff? ")
+        while not (reset_input.lower() == "n"):
+            print("\nError>> that is not even an option \n\n *****please try again*****\n")
+            reset_input = input("Do you want to decode/encode more stuff? (y/n) ")
         # takes user to the beginning of the program when user wants to encode/decode again
-            if (reset_input.lower() == "yes" or reset_input.lower() == "sure"):
+            if (reset_input.lower() == "y"):
                 b64_program()
                 break
          # program ends when user does not want to encode/decode
-            if (reset_input.lower() == "no" or reset_input.lower() == "nope"):
+            if (reset_input.lower() == "n"):
                 print("bye, thanks for using this")
                 input("press enter to exit")
                 break
